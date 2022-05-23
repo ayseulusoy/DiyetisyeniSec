cf0a0c38856609ee0de52802c3910863e68f16dc
ayseulusoy <u-l-u-s-o-y-15@hotmail.com>
2022-05-23 14:01:46 +0300

    Delete and Edit

--- DiyetisyeniSec.Web/Controllers/NutritionistController.cs
+++ DiyetisyeniSec.Web/Controllers/NutritionistController.cs
@@ -19,7 +19,6 @@ namespace DiyetisyeniSec.Web.Controllers
         }
 
         //GET
-        [HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -43,6 +42,66 @@ namespace DiyetisyeniSec.Web.Controllers
                 return RedirectToAction("Index");
             }
             return View(nutritionist);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            var diyetisyen= _context.Nutritionists.FirstOrDefault(x => x.Id == id);
+            if (diyetisyen==null)
+            {
+                return NotFound();
+            }
+            return View(diyetisyen);
+            /*
+           _context.Nutritionists.Find(id);
+           _context.Nutritionists.FirstOrDefault(x => x.Id == id);
+           _context.Nutritionists.SingleOrDefault(x => x.Id == id);
+           */
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Güvenlik için gerekli istek sahteciliğini önler
+        public IActionResult Edit(Nutritionist nutritionist)
+        {
+            if (nutritionist.Experience < 4 && nutritionist.Experience > 2 && nutritionist.UniversityName != "Akdeniz Universitesi")
+            {
+                ModelState.AddModelError("experience", "Eğer Akdeniz Üniversitesi Mezunu Değilseniz En Az 4 Yıl Tecübeniz Olmak Zorunda.");
+                //küçük modal ismini yazıp yaprsak altta olur hata mesajı
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Nutritionists.Update(nutritionist);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nutritionist);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var diyetisyen= _context.Nutritionists.FirstOrDefault(x => x.Id==id);
+            if (diyetisyen==null)
+            {
+                return NotFound();
+            }
+            
+            return View(diyetisyen);
+        }
+        [HttpPost,ActionName("Delete")] //ActionName bir attribute tags örneğidir.
+        public IActionResult DeletePost(int? id)
+        {
+            var diyetisyen = _context.Nutritionists.FirstOrDefault(x => x.Id == id);
+            if (diyetisyen == null)
+            {
+                return NotFound();
+            }
+            _context.Nutritionists.Remove(diyetisyen);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
